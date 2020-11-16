a10ac502586c251ea35a3c30edae0dba52ae1917
Akhil Garg <akhilgarg420@gmail.com>
2020-11-17 05:23:24 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String op = "y";
+        while(true){
+            if(op.equals("y") || op.equals(("Y"))) {
+                enterDetails();
+            } else if(op.equals("n") || op.equals("N")){
+                System.out.println("Thanks for using tax calculator");
+                break;
+            } else{
+                System.out.println("Please enter either y or n");
+            }
+            System.out.println("Do you want to enter details of any other item (y/n):");
+            op = sc.nextLine();
+
+        }
+
+    }
+
+    private static void enterDetails() {
+        String name;
+        String type;
+        String quantity;
+        double price;
+        double[] res;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter item name => ");
+        name = sc.nextLine();
+        System.out.print("Choose between the three \nRaw \nManufactured \nImported\n");
+        type = enterType();
+        System.out.print("Enter quantity => ");
+        quantity = sc.nextLine();
+        System.out.print("Enter price => ");
+        price = sc.nextDouble();
+        res = calTax(price,type);
+
+        String s1 = String.format("Item name => %s \nPrice=> %.2f \nTax => %.2f \nOverall Effective Cost => %.2f",name,price,res[0],res[1]);
+        System.out.println(s1);
+
+    }
+
+    private static String enterType() {
+        String type;
+        Scanner sc= new Scanner(System.in);
+        type=sc.nextLine();
+        while(true){
+            if(type.equals("Raw")||type.equals("Imported")||type.equals("Manufactured")){
+                return type;
+            }
+            else{
+                System.out.println("Please enter the correct type");
+                type = sc.nextLine();
+            }
+        }
+    }
+
+    private static double[] calTax(double price, String type) {
+        double res[] = new double[2];
+        double tax=0;
+        double effectiveCost=0;
+        double rawTax = 12.5*(price/100);
+        double manTax = rawTax+2*((price+rawTax)/100);
+        double impTax = 10*(price/100);
+        if(type.equals("Raw")){
+            tax = rawTax;
+            effectiveCost = price+tax;
+        } else if(type.equals("Manufactured")){
+            tax = rawTax+2*((price+rawTax)/100);
+            effectiveCost = price+tax;
+        }else if(type.equals("Imported")){
+            effectiveCost= price+impTax;
+            if (effectiveCost <= 100) {
+                effectiveCost += 5;
+                tax = impTax + 5;
+            }
+            else if (effectiveCost > 100 && effectiveCost <= 200) {
+                effectiveCost += 10;
+                tax = impTax + 5;
+            }
+            else if (effectiveCost > 200) {
+                tax = impTax + 5*(effectiveCost/100);
+                effectiveCost += 5 * (effectiveCost / 100);
+            }
+        }
+        res[0]= tax;
+        res[1] = effectiveCost;
+        return res;
+
+    }
+
+
+}
